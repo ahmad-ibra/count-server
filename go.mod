e4e2856c41150078351e21667146d8860255529a
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2023-12-11 10:00:39 -0800

    Implement the CountService

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/ahmad-ibra/count-server
 
 go 1.21.3
+
+require (
+	connectrpc.com/connect v1.13.0
+	golang.org/x/net v0.19.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require golang.org/x/text v0.14.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+
+	connect "connectrpc.com/connect"
+	countv1 "github.com/ahmad-ibra/count-server/gen/count/v1"
+	"github.com/ahmad-ibra/count-server/gen/count/v1/countv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
+const address = "localhost:8080"
+
+var count = 0
+
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	path, handler := countv1connect.NewCountServiceHandler(&countServiceServer{})
+	mux.Handle(path, handler)
+	fmt.Printf("Listening on %v...\n", address)
+	http.ListenAndServe(
+		address,
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
+
+// countServiceServer implements the CountService API.
+type countServiceServer struct {
+	countv1connect.UnimplementedCountServiceHandler
+}
+
+// Count counts the number of times it has been called.
+func (s *countServiceServer) Count(
+	ctx context.Context,
+	req *connect.Request[countv1.CountRequest],
+) (*connect.Response[countv1.CountResponse], error) {
+	count++
+	log.Printf("Got a request to count: %d", count)
+	return connect.NewResponse(&countv1.CountResponse{}), nil
 }
